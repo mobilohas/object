32173849d46797dd5d81b191a1d155e1d48ce48b
pythonstrup <pythonstrup@gmail.com>
2024-07-09 00:00:22 +0900

    bell - chapter04: 리팩토링2-2 template 활용

--- example/src/main/java/chapter04/bell/refactoring2/AmountDiscountPolicy.java
+++ example/src/main/java/chapter04/bell/refactoring2/AmountDiscountPolicy.java
@@ -7,17 +7,15 @@ public class AmountDiscountPolicy extends DiscountPolicy {
 
   private Money discountAmount;
 
-  public AmountDiscountPolicy(final Money discountAmount, final List<DiscountCondition> discountConditions) {
+  public AmountDiscountPolicy(final Money discountAmount,
+      final List<DiscountCondition> discountConditions) {
     super(discountConditions);
     this.discountAmount = discountAmount;
   }
 
   @Override
-  public Money calculateDiscountAmount(
+  public Money calculateDiscountPolicyAmount(
       final LocalDateTime whenScreened, final int sequence, final Money movieFee) {
-    if (isDiscountable(whenScreened, sequence)) {
-      return discountAmount;
-    }
-    return Money.ZERO;
+    return discountAmount;
   }
 }

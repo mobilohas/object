295220e06c321b14c33ade6cbe424f44afe3551b
pythonstrup <pythonstrup@gmail.com>
2024-07-23 00:12:59 +0900

    bell - chapter06 Event 개선

---
+++ example/src/main/java/chapter06/bell/improvement/Event.java
@@ -0,0 +1,34 @@
+package chapter06.bell.improvement;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Event {
+  private String subject;
+  private LocalDateTime from;
+  private Duration duration;
+
+  public Event(String subject, LocalDateTime from, Duration duration) {
+    this.subject = subject;
+    this.from = from;
+    this.duration = duration;
+  }
+
+  public boolean isSatisfied(final RecurringSchedule schedule) {
+    if (from.getDayOfWeek() != schedule.getDayOfWeek()
+    || !from.toLocalTime().equals(schedule.getFrom())
+    || !duration.equals(schedule.getDuration())) {
+      return false;
+    }
+    return true;
+  }
+
+  public void reschedule(final RecurringSchedule schedule) {
+    from = LocalDateTime.of(from.toLocalDate().plusDays(daysDistance(schedule)), schedule.getFrom());
+    duration = schedule.getDuration();
+  }
+
+  private long daysDistance(final RecurringSchedule schedule) {
+    return schedule.getDayOfWeek().getValue() - from.getDayOfWeek().getValue();
+  }
+}

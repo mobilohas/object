116a04deefec641043952689deb2bff387d3d7ce
pythonstrup <pythonstrup@gmail.com>
2024-08-31 21:09:48 +0900

    bell - chapter11 단순 상속 예제: 결합도가 높다

---
+++ example/src/main/java/chapter11/bell/phoneinheritance/NightlyDiscountPhone.java
@@ -0,0 +1,26 @@
+package chapter11.bell.phoneinheritance;
+
+import java.time.Duration;
+
+public class NightlyDiscountPhone extends Phone {
+  private static final int LATE_NIGHT_HOUR = 22;
+
+  private Money nightlyAmount;
+  private Money regularAmount;
+  private Duration seconds;
+
+  public NightlyDiscountPhone(final Money nightlyAmount, final Money regularAmount,
+      final Duration seconds) {
+    this.nightlyAmount = nightlyAmount;
+    this.regularAmount = regularAmount;
+    this.seconds = seconds;
+  }
+
+  @Override
+  protected Money calculateCallFee(Call call) {
+    if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+      return nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds());
+    }
+    return regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds());
+  }
+}

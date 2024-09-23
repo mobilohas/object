bce65888b96295b385cb9ca3872797f98a5ed615
pythonstrup <pythonstrup@gmail.com>
2024-09-23 23:00:41 +0900

    bell - chapter14 협력 패턴 설계하기

---
+++ example/src/main/java/chapter14/bell/consitent/TimeOfDayFeeCondition.java
@@ -0,0 +1,37 @@
+package chapter14.bell.consitent;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TimeOfDayFeeCondition implements FeeCondition {
+  private LocalTime from;
+  private LocalTime to;
+
+  public TimeOfDayFeeCondition(final LocalTime from, final LocalTime to) {
+    this.from = from;
+    this.to = to;
+  }
+
+  @Override
+  public List<DateTimeInterval> findTimeIntervals(final Call call) {
+    return call.getInterval().splitByDay()
+        .stream()
+        .map(each ->
+            DateTimeInterval.of(
+                LocalDateTime.of(each.getFrom().toLocalDate(), from(each)),
+                LocalDateTime.of(each.getTo().toLocalDate(), to(each))))
+        .toList();
+  }
+
+  private LocalTime from(final DateTimeInterval interval) {
+    return interval.getFrom().toLocalTime().isBefore(from) ?
+        from: interval.getFrom().toLocalTime();
+  }
+
+  private LocalTime to(final DateTimeInterval interval) {
+    return interval.getTo().toLocalTime().isAfter(to) ?
+        to: interval.getTo().toLocalTime();
+  }
+}
